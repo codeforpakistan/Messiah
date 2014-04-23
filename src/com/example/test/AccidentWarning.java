7dc9144db93b96cfcca44ff0660a660d4aedcddf
zahid--ali <zfz.zahid@yahoo.com>
2014-04-23 15:19:47 +0500

    I am safe  and accident warning added
    this functionality will send sms to the contacts if u are safe

--- src/com/example/test/AccidentWarning.java
+++ src/com/example/test/AccidentWarning.java
@@ -1,22 +1,34 @@
 package com.example.test;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.Menu;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class AccidentWarning extends Activity {
 	 MediaPlayer mp ;
-	
+	 AppLocationService appLocationService;
+	 String lat=null;
+	 String lon=null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_accident_warning);
-		
+		appLocationService = new AppLocationService(AccidentWarning.this);
 		
 	mp	= MediaPlayer.create(AccidentWarning.this, R.raw.alarm);	
 		notification();
@@ -42,6 +54,8 @@ public class AccidentWarning extends Activity {
 					public void onClick(DialogInterface dialog,
 							int whichButton) {
 						dialog.cancel();
+						mp.stop();
+						AccidentWarning.this.finish();
 					}
 					// End of onClick(DialogInterface dialog, int
 					// whichButton)
@@ -51,8 +65,7 @@ public class AccidentWarning extends Activity {
 					public void onClick(DialogInterface dialog,
 							int whichButton) {
 						// Canceled.
-						dialog.cancel();
-					mp.stop();
+					
 						
 					}
 				}); // End of alert.setNegativeButton
@@ -69,6 +82,65 @@ Toast.makeText(getApplicationContext(), "accident", Toast.LENGTH_SHORT).show();
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.accident_warning, menu);
 		return true;
+	}
+	public void SendSMS()
+	{
+		 
+		try{
+			Location nwLocation = appLocationService
+					.getLocation(LocationManager.NETWORK_PROVIDER);
+
+			if (nwLocation != null) {
+				double latitude = nwLocation.getLatitude();
+				double longitude = nwLocation.getLongitude();
+				
+				lon = String.valueOf(longitude);
+				lat = String.valueOf(latitude);
+				String add = GetAddress(lat, lon);
+				Toast.makeText(
+						getApplicationContext(),
+						"Mobile Location (NW): \nLatitude: " + latitude
+								+ "\nLongitude: " + longitude + " " + add,
+						Toast.LENGTH_SHORT).show();
+			
+		}
+
+			String pinpoint = "http://www.maps.google.com/maps?q="+lat+","+lon;
+			String address = GetAddress(lat, lon);
+			DataInsertion obj = new DataInsertion();
+			String[] phonenumber = obj.getphonenumbers(getApplicationContext());
+			String[] messages = obj.getmessages(getApplicationContext());
+			for(int i = 0; i<= phonenumber.length;i++ )
+			SmsManager.getDefault().sendTextMessage(phonenumber[i], null, messages[i] + "Im at: " + address + " " + pinpoint, null, null);
+		}
+		catch(Exception ex)
+		{
+			Log.d("SMS Error: ", ex.getMessage().toString());
+		}
+	}
+	public String GetAddress(String lat, String lon)
+	{
+		Geocoder geocoder = new Geocoder(this, Locale.ENGLISH);
+		String ret = "";
+		try {
+			List<Address> addresses = geocoder.getFromLocation(Double.parseDouble(lat), Double.parseDouble(lon), 1);
+			if(addresses != null) {
+				Address returnedAddress = addresses.get(0);
+				StringBuilder strReturnedAddress = new StringBuilder("Address:\n");
+				for(int i=0; i<returnedAddress.getMaxAddressLineIndex(); i++) {
+					strReturnedAddress.append(returnedAddress.getAddressLine(i)).append("\n");
+				}
+				ret = strReturnedAddress.toString();
+			}
+			else{
+				ret = null;
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			ret = null;
+		}
+		return ret;
 	}
 
 }
