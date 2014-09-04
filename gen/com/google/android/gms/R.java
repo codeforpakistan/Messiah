c41ca1088f163c88429f1dcad400a74ae700350c
zahid--ali <zfz.zahid@yahoo.com>
2014-09-05 02:02:56 +0500

    UI Optimization Part-II

--- gen/com/google/android/gms/R.java
+++ gen/com/google/android/gms/R.java
@@ -99,8 +99,8 @@ public final class R {
 		public static final int ic_plusone_small_off_client = 0x7f020079;
 		public static final int ic_plusone_standard_off_client = 0x7f02007a;
 		public static final int ic_plusone_tall_off_client = 0x7f02007b;
-		public static final int powered_by_google_dark = 0x7f020080;
-		public static final int powered_by_google_light = 0x7f020081;
+		public static final int powered_by_google_dark = 0x7f020083;
+		public static final int powered_by_google_light = 0x7f020084;
 	}
 	public static final class id {
 		public static final int book_now = 0x7f060028;
